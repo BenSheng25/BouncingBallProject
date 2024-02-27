2a067ac1cc1e11e1b4b0ec2d64721a08c98fd71e
JacksonDing123 <122178040+JacksonDingJD@users.noreply.github.com>
2024-02-27 10:56:00 -0500

    push for ben initial code added

--- BouncingBallProject/src/Main.java
+++ BouncingBallProject/src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        int x = input.nextInt();
+        int y = input.nextInt();
+        int p = input.nextInt();
+        int q = input.nextInt();
+
+        double ballx = p;
+        double bally = 0;
+
+        double xVelocity = 0.02;
+        double yVelocity = xVelocity*((double)q/p);
+
+        int ballBounceCount = 0;
+
+        while(!((ballx>x-5&&bally<5)||(ballx>x-5&&bally>y-5)||(ballx<5&&bally>y-5)||(ballx<5&&bally<5))){
+            if((x-ballx)<0.02||ballx<0.02){
+                xVelocity = -xVelocity;
+                ballBounceCount++;
+            }
+            if((y-bally)<0.02||bally<0.02){
+                xVelocity = -xVelocity;
+                ballBounceCount++;
+            }
+            ballx+=xVelocity;
+            bally+=yVelocity;
+        }
+        System.out.println(ballBounceCount);
     }
 }
